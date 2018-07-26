673cd427512208a6edb45f351b61c210c9b81628
jamieberrier <berrier.jamie@gmail.com>
2018-07-26 02:48:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,11 @@
 def oxford_comma(array)
-  array.join(", ")
 
-if array.size == 1
-  array.join
-elsif array.size == 2
-  array.join(" and ")
-else array.size >= 3
-  #array.join
-end
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else array.size >= 3
+    #array.join
+  end
 
 end
