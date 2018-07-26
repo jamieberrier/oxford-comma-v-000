fecbeafb287aca82870c4016b5ec99488f6baf87
jamieberrier <berrier.jamie@gmail.com>
2018-07-26 03:06:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,9 +9,9 @@ def oxford_comma(array)
     array.each do |word|
       while array.size > counter
         word << ", "
-      end
         counter += 1
-        word = "and " + word
+      end
+                word = "and " + word
     end
    end
 
